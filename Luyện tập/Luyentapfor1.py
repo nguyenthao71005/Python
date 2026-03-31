6f5f6c3b62cf151f56f2410b3806674fe2130b5c
Nguyễn Phương Thảo <nguyenthao7102005@gmail.com>
2026-03-31 10:58:27 +0700

    Delete Luyện tập directory

--- Luyện tập/Luyentapfor1.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Nhập vào n: "))
-for _i in range(1,n):
-    print("2 * %d = %d" % (_i, 2 * _i))
--- Luyện tập/Luyentapfor3.py
+++
@@ -1,3 +0,0 @@
-for i in range(80, 101):
-    if i % 2 == 0 and i % 3 == 0:
-        print(i)
